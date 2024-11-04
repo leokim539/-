cfa8500026aee7e8ffeffcfd679f05ac5e0c8ba9
kyj0616 <youngjin379@yeonsung.ac.kr>
2024-11-05 02:49:03 +0900

    TV, 서랍, 핸드폰 이벤트 90퍼 완성
    트리거 진입 이벤트 간 쿨타임 적용 아직 안됨. 내일 학교 갔다와서 할게요.

---
+++ Assets/Scripts/ColliderEvent/CellphoneEvent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellphoneEvent : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // 이 오브젝트의 자식 오브젝트를 순회
+            foreach (Transform child in transform)
+            {
+                // 자식 오브젝트 활성화
+                child.gameObject.SetActive(true);
+
+                // 자식 오브젝트의 자식 오브젝트 활성화
+                foreach (Transform grandChild in child)
+                {
+                    grandChild.gameObject.SetActive(true);
+
+                    // AudioSource가 있는 경우 재생
+                    AudioSource audioSource = grandChild.GetComponent<AudioSource>();
+                    if (audioSource != null)
+                    {
+                        audioSource.Play();
+                    }
+                }
+            }
+        }
+    }
+}
+
