cd34aecb229c9a16a2dbabd25460813ec303919d
kyj0616 <youngjin379@yeonsung.ac.kr>
2024-11-09 17:24:41 +0900

    벽 두들기는 소리
    특정 부분 지나가면 벽 두들기는 소리 나는 이벤트

---
+++ Assets/Scripts/ColliderEvent/WallHit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallHit : MonoBehaviour
+{
+  private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
