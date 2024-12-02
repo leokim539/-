646e86c1849f65133970a62fbabdbe96e782c249
PanHuHi <yunoyj70@gmail.com>
2024-12-03 03:45:27 +0900

    .

--- Assets/Scripts/Menu/OptionsMenu.cs
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -1,38 +1,47 @@
 using UnityEngine;
 using UnityEngine.UI;
-
-public class OptionsMenuController : MonoBehaviour
+using TMPro;
+public class OptionsMenu : MonoBehaviour
 {
-    public GameObject optionsPanel; // OptionsPanel을 연결
-    public Button openOptionsButton; // 옵션 열기 버튼
-    public Button closeButton; // 닫기 버튼
-    public Button exitButton; // Exit 버튼
+    [Header("Panels")]
+    public GameObject optionsPanel;
+    public GameObject tutorialPanel;
+
+    [Header("Buttons")]
+    public Button openOptionsButton;
+    public Button closeOptionsButton;
+    public Button exitButton;
+    public Button openTutorialButton;
+    public Button closeTutorialButton;
 
     void Start()
     {
-        // 옵션 창 비활성화
-        optionsPanel.SetActive(false);
+        // 패널 비활성화
+        SetPanelActive(optionsPanel, false);
+        SetPanelActive(tutorialPanel, false);
 
-        // 각 버튼에 이벤트 연결
-        openOptionsButton.onClick.AddListener(OpenOptionsPanel);
-        closeButton.onClick.AddListener(CloseOptionsPanel);
-        exitButton.onClick.AddListener(ExitGame); // Exit 버튼에 이벤트 연결
+        // 버튼 이벤트 설정
+        AddButtonListener(openOptionsButton, () => SetPanelActive(optionsPanel, true));
+        AddButtonListener(closeOptionsButton, () => SetPanelActive(optionsPanel, false));
+        AddButtonListener(openTutorialButton, () => SetPanelActive(tutorialPanel, true));
+        AddButtonListener(closeTutorialButton, () => SetPanelActive(tutorialPanel, false));
+        AddButtonListener(exitButton, ExitGame);
     }
 
-    void OpenOptionsPanel()
+    void SetPanelActive(GameObject panel, bool isActive)
     {
-        optionsPanel.SetActive(true); // 옵션 창 활성화
+        if (panel != null)
+            panel.SetActive(isActive);
     }
 
-    void CloseOptionsPanel()
+    void AddButtonListener(Button button, UnityEngine.Events.UnityAction action)
     {
-        optionsPanel.SetActive(false); // 옵션 창 비활성화
+        if (button != null)
+            button.onClick.AddListener(action);
     }
 
     void ExitGame()
     {
-        Application.Quit(); // 게임 종료
-
-       
+        Application.Quit();
     }
 }
