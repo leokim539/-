60ad072e89bcdc30d331ffdd6abd99efa436f423
kyj0616 <youngjin379@yeonsung.ac.kr>
2024-12-05 16:17:21 +0900

    서랍 동기화 완료
    ..

--- Assets/Scripts/ColliderEvent/DrawerEventTrigger.cs
+++ Assets/Scripts/ColliderEvent/DrawerEventTrigger.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class DrawerEventTrigger : MonoBehaviour
 {
+    private bool isTriggered = false; // 트리거가 실행되었는지 여부를 나타내는 변수
+
     private void OnTriggerEnter(Collider other)
     {
         // 플레이어와 충돌했는지 확인
-        if (other.CompareTag("Player"))
+        if (!isTriggered && other.CompareTag("Player")) // 이미 트리거가 실행되지 않았을 때만
         {
             // 자식 오브젝트 중 "OutoDrawer"라는 이름의 오브젝트 찾기
             Transform outoDrawerTransform = transform.Find("OutoDrawer");
@@ -19,6 +21,7 @@ public class DrawerEventTrigger : MonoBehaviour
                 {
                     // 서랍 열기 기능 실행
                     StartCoroutine(drawerController.ToggleDrawer());
+                    isTriggered = true; // 트리거가 실행되었음을 표시
                 }
                 else
                 {
