b8e64cf3fdef4a47486813b3b62f3c3f3df24d27
PanHuHi <>
2024-12-10 16:55:23 +0900

    1234

--- Assets/Prefab/realTrash/PetBottleObjectData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewPetBottleObject", menuName = "Trash/PetBottleObjectData")]
-public class PetBottleObjectData : ScriptableObject
-{
-    public GameObject prefab; // PET º´ ÇÁ¸®ÆÕ
-}
--- Assets/Prefab/realTrash/TrashBagObjectData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewTrashBagObject", menuName = "Trash/TrashBagObjectData")]
-public class TrashBagObjectData : ScriptableObject
-{
-    public GameObject prefab; // æ≤∑π±‚ ∫¿≈ı «¡∏Æ∆’
-}
