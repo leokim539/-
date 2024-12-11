eb19636b709be61ea0c471941174f2e00ebd1dfc
kyj0616 <youngjin379@yeonsung.ac.kr>
2024-12-11 15:23:51 +0900

    제발 되어라
    .

--- Assets/Scripts/Ending관련/TrashCount.cs
+++ Assets/Scripts/Ending관련/TrashCount.cs
@@ -22,7 +22,7 @@ public class TrashCount : MonoBehaviourPunCallbacks
             totalTrashCount += count;
             LogTrashCount(); // 쓰레기 수 로그 출력
 
-            // 모든 플레이어의 정보를 업데이트
+            // 모든 플레이어의 쓰레기 수를 업데이트
             UpdateAllPlayerInfos();
         }
     }
@@ -35,9 +35,14 @@ public class TrashCount : MonoBehaviourPunCallbacks
             PlayerInfo[] playerInfos = new PlayerInfo[PhotonNetwork.PlayerList.Length];
             for (int i = 0; i < playerInfos.Length; i++)
             {
-                // 각 플레이어의 쓰레기 수를 가져오기
-                int trashCount = (i + 1 == photonView.Owner.ActorNumber) ? totalTrashCount : 0; // 이 플레이어의 쓰레기 수
-                playerInfos[i] = new PlayerInfo(PhotonNetwork.PlayerList[i].NickName, trashCount);
+                string playerName = PhotonNetwork.PlayerList[i].NickName;
+
+                // 각 플레이어의 쓰레기 수를 저장
+                int trashCount = (PhotonNetwork.PlayerList[i].ActorNumber == photonView.Owner.ActorNumber)
+                                 ? totalTrashCount
+                                 : 0; // 상대방의 쓰레기 수는 0으로 초기화
+
+                playerInfos[i] = new PlayerInfo(playerName, trashCount);
             }
             resultUIManager.UpdateResult(playerInfos);
         }
