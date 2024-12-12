3b0d4bf857024bc06f3f5518ae8942a1298572a1
kyj0616 <youngjin379@yeonsung.ac.kr>
2024-12-12 15:53:16 +0900

    ㅂㅈㄷㄱ

--- Assets/Prefab/realTrash/BeerCanObjectData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewBeerCanObject", menuName = "Trash/BeerCanObjectData")]
-public class BeerCanObjectData : ScriptableObject
-{
-    public GameObject prefab; // ¸ÆÁÖ Äµ ÇÁ¸®ÆÕ
-}
